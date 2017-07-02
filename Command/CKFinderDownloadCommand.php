d373e12c46264ac06141c7e507a159b8e904d5f4
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2017-07-02 10:43:33 +0200

    Update for 3.4.2.

--- Command/CKFinderDownloadCommand.php
+++ Command/CKFinderDownloadCommand.php
@@ -25,7 +25,7 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class CKFinderDownloadCommand extends ContainerAwareCommand
 {
-    const ZIP_PACKAGE_URL = 'http://download.cksource.com/CKFinder/CKFinder%20for%20PHP/3.4.0/ckfinder_php_3.4.0.zip';
+    const ZIP_PACKAGE_URL = 'http://download.cksource.com/CKFinder/CKFinder%20for%20PHP/3.4.2/ckfinder_php_3.4.2.zip';
 
     /**
      * {@inheritdoc}
