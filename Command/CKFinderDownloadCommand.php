0b2288d90986430d6571cf7afde067c62c3dfde5
Pawel Wiaderny <zaak@users.noreply.github.com>
2018-11-27 15:29:17 +0100

    Update to v3.4.5

--- Command/CKFinderDownloadCommand.php
+++ Command/CKFinderDownloadCommand.php
@@ -25,7 +25,7 @@ use Symfony\Component\Filesystem\Filesystem;
  */
 class CKFinderDownloadCommand extends Command
 {
-    const ZIP_PACKAGE_URL = 'http://download.cksource.com/CKFinder/CKFinder%20for%20PHP/3.4.4/ckfinder_php_3.4.4.zip';
+    const ZIP_PACKAGE_URL = 'http://download.cksource.com/CKFinder/CKFinder%20for%20PHP/3.4.5/ckfinder_php_3.4.5.zip';
 
     /**
      * {@inheritdoc}
